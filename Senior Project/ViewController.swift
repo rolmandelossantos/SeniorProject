fec9d795ac06e003ff85c4e55be927cc8aac1aa5
Rolman De los Santos <rolman.delossantos@gmail.com>
2018-03-26 11:23:36 -0400

    Initial Commit

---
+++ Senior Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Senior Project
+//
+//  Created by Rolman De los Santos on 3/26/18.
+//  Copyright © 2018 Rolman De los Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
