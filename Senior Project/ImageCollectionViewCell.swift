75d0721aa4b8ee6d251e27a56711ee96f974cc25
Rolman De los Santos <rolman.delossantos@gmail.com>
2018-03-27 01:53:03 -0400

    gallerytab

---
+++ Senior Project/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  Senior Project
+//
+//  Created by Rolman De los Santos on 3/27/18.
+//  Copyright © 2018 Rolman De los Santos. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImgImage: UIImageView!
+}
--- Senior Project/ImageViewController.swift
+++ Senior Project/ImageViewController.swift
@@ -1,16 +1,16 @@
 //
-//  ViewController.swift
+//  ImageViewController.swift
 //  Senior Project
 //
-//  Created by Rolman De los Santos on 3/26/18.
+//  Created by Rolman De los Santos on 3/27/18.
 //  Copyright © 2018 Rolman De los Santos. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ImageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    var imageArray = [UIImage(named:"calendar"),UIImage(named:"clipper")]
+    var imageArray = [UIImage(named:"1"),UIImage(named:"2"),UIImage(named:"3"),UIImage(named:"4"),UIImage(named:"5")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +21,25 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath)
-        as! ImageCollectionViewCell
+            as! ImageCollectionViewCell
         
         cell.ImgImage.image = imageArray[indexPath.row]
         
         return cell
         
     }
-
+    
 }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 
